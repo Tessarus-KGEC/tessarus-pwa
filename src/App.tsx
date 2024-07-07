6086f175590423d2410ea8f8c70d6c757847e91d
The Dakshi <kdakshi2020@gmail.com>
2024-07-07 19:04:27 +0530

    nextui setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import { useEffect, useState } from 'react'
-import './App.css'
-import reactLogo from './assets/react.svg'
-import appLogo from '/favicon.svg'
-
+import { Button } from '@nextui-org/button';
+import { NextUIProvider } from '@nextui-org/system';
+import { useEffect, useState } from 'react';
+import reactLogo from './assets/react.svg';
+import appLogo from '/favicon.svg';
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   useEffect(() => {
     if ('serviceWorker' in navigator && 'PushManager' in window) {
@@ -28,32 +28,31 @@ function App() {
           console.error('Service Worker registration failed', error);
         });
     }
-  }, [])
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={appLogo} className="logo" alt="tessarus-pwa logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1 className='text-orange-300'>tessarus-pwa</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <NextUIProvider>
+      <main className="bg-background text-foreground dark">
+        <div>
+          <a href="https://vitejs.dev" target="_blank">
+            <img src={appLogo} className="logo" alt="tessarus-pwa logo" />
+          </a>
+          <a href="https://react.dev" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
+        </div>
+        <h1 className="text-orange-300">tessarus-pwa</h1>
+        <div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+          <p>
+            Edit <code>src/App.tsx</code> and save to test HMR
+          </p>
+          <Button>NextUI Button</Button>
+        </div>
+        <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      </main>
+    </NextUIProvider>
+  );
 }
 
-export default App
+export default App;
